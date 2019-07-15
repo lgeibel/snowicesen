add53be061706c497654b54c58556befff19ada1
Lea Geibel <geibell@vierzack03.baug.ethz.ch>
2019-07-15 15:31:29 +0200

    create_validation_set.py updated

--- create_validation_set.py
+++ create_validation_set.py
@@ -1,29 +1,20 @@
 import warnings
 warnings.filterwarnings('ignore')
 from snowicesen import cfg
-from snowicesen import tasks
 import geopandas as gpd
 import logging
 from snowicesen.workflow import init_glacier_regions
 import xarray as xr
 import random
-from snowicesen.utils import download_all_tiles
-from oggm.workflow import execute_entity_task
-from datetime import timedelta
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy.misc
+import rasterio
 import os
-from snowicesen.utils import datetime_to_int, int_to_datetime, extract_metadata
 
 
 logging.basicConfig(format='%(asctime)s: %(name)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG)
 log = logging.getLogger(__name__)
 log.setLevel('DEBUG')
 
-cfg.initialize("/scratch_net/vierzack03_third/geibell/snowicesen/snowicesat_params.cfg")
-# Caution: In crampon.utils.GlacierDirectory._init cfg.initialize is called again --> change path there as well!
-#cfg.PATHS['working_dir'] = r"C:\Users\Lea Geibel\Documents\ETH\MASTERTHESIS\snowicesat\SWISS"
+cfg.initialize("/scratch_net/vierzack03_third/geibell/snowicesen/snowicesen_params.cfg")
 cfg.PATHS['dem_dir'] = "/scratch_net/vierzack03_third/geibell/snowicesen/data/DEM/SWISSALTI3D_2018"
 
     # something new in OGGM that we are not yet able to handle
@@ -33,19 +24,18 @@ cfg.PARAMS['use_multiprocessing'] = False
 cfg.PARAMS['mp_processes'] = 4
 
 if __name__ == '__main__':
-    samples = 30 # number of samples that we want
     rgidf = gpd.read_file("/scratch_net/vierzack03_third/geibell/snowicesen/data/outlines/rgi_copy_status.shp")
     # Ignore all values glaciers smaller than 0.1 km^2
     rgidf = rgidf.loc[rgidf['Area'] > 0.1]
-
-    rgidf = rgidf.sample(n=100)
+    rgidf = rgidf[rgidf.RGIId.isin(['RGI50-11.B8315n'])]  ##########
     gdirs = init_glacier_regions(rgidf, reset= False, force=False)
-    # Total area:
+    # Total area
     area_tot = sum(rgidf.Area)
     # Weight by area:
     weight = (rgidf.Area/area_tot).tolist()
     rgidf.RGIId
 
+    samples =1  # number of samples for this list #########
     my_list = []
     for num, name in enumerate(rgidf.RGIId):
         my_list.append([name] * round(weight[num] * samples))
@@ -61,27 +51,38 @@ if __name__ == '__main__':
         gdir = gdirs[index]
 
         # Read file, get list of all available dates:
-        snow = xr.open_dataset(gdir.get_filepath('snow_cover'))
 
-        # get a random date from all dates for this glacier:
-        rand_date  = random.choice(snow.time.values)
-        print(snow.time.values)
-        print(rand_date)
+        #read
+        sentinel = xr.open_dataset(gdir.get_filepath('sentinel')) ##########
+        rand_date = 20170907 # random.choice(sentinel.time.values) ########
+        glacier = gpd.read_file(gdir.get_filepath('outlines'))
+        DEM = rasterio.open(os.path.join(cfg.PATHS['dem_dir'], 'swissAlti3D_lv03_ln02_r2018_bilinear10m_UTM32.tif'))
+        out_meta = DEM.profile
+        print(sentinel.img_values.attrs)
+        
 
-        # Read corresponding sentinel file:
-        sentinel = xr.open_dataset(gdir.get_filepath('sentinel_temp'))
+        # Update Metadata
+        out_meta['crs'] = sentinel.img_values.attrs['crs']
+        out_meta['count'] = 3
+        try:
+            out_meta['transform'] = rasterio.transform.guard_transform(sentinel.img_values.attrs['transform'])
+        except:
+            del out_meta['transform']
+        out_meta['height'] = sentinel.img_values.y.values.size
+        out_meta['width'] = sentinel.img_values.x.values.size
+        out_meta['dtype'] = 'uint16'
+        out_meta['nodata'] = 65535
 
-        # write into RGB image
-        b04 = sentinel.sel(band = 'B04', time = rand_date).img_values.values/10000
-        b03 = sentinel.sel(band = 'B03', time = rand_date).img_values.values/10000
-        b02 = sentinel.sel(band = 'B02', time = rand_date).img_values.values/10000
+        file_list = ['B02','B8A', 'B08']
 
-        rgb_image = np.array([b04, b03, b02]).transpose((1,2,0))
-        rgb_image[rgb_image > 1] = 1
+        # Write to GeoTiff
+        with rasterio.open(os.path.join(cfg.PATHS['working_dir'],'RGB', str(rand_RGIId+'_'+ str(rand_date)+'.tif')), 'w', **out_meta) as dst:
+            for id,layer in enumerate(file_list, start=1):
+                dst.write_band(id, sentinel.sel(time=rand_date, band=layer).img_values.values)
 
-        # RGB to file:
-        filename = os.path.join(cfg.PATHS['working_dir'], 'RGB', str(rand_RGIId) +'_' + str(rand_date)+'.jpg')
-        scipy.misc.imsave(filename, rgb_image)
+
+    #print(sentinel.time)
+
 
 
 
